1c54a086af3301d1d49816942cf43d871e3e0465
LKCSFR <laurent.koese@student.uva.nl>
2025-04-17 13:26:10 +0200

    banch laatste

--- test.py
+++ test.py
@@ -4,8 +4,3 @@ print("Skrrr pap pap")
 print("dit is een eerste aanpassing")
 print("Zodaaan encroyable")
 print("dit is een sluipmoordernaar")
-
-het is weer een mooie dag
-xxsubtype
-
-wet ik veel
